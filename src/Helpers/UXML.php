d9773909e015ac8522cb9301894af7a68fb90e36
Zaher Thaifani <zaher@salla.sa>
2024-07-20 20:31:28 +0300

    Update UXML.php

--- src/Helpers/UXML.php
+++ src/Helpers/UXML.php
@@ -325,6 +325,7 @@ class UXML
      *
      * @param Certificate $certificate
      * @param string $invoiceHash the base64 encoded string of the binary invoice hash
+     * @param string $digitalSignature 
      * @return array
      */
     public function toTagsArray(Certificate $certificate, string $invoiceHash, string $digitalSignature): array
